6c6e4ac259d158bc2ad01c43adac9278a681394a
Paul Joy <pjoyjr@corsair.cs.iupui.edu>
2018-11-01 13:51:31 -0400

    blank page added for easy manipulation

---
+++ blankPage.php
@@ -0,0 +1,12 @@
+<?php 
+require_once 'header.php';
+?>
+
+<div class="container" id="blankPage">
+	<section id="blankHeader">
+		<h2>Blank Page</h2>
+	</section>
+	
+</div>
+
+<?php include 'footer.php'; ?>
