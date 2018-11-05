dd9cf6230100431244c034ef2ce922d231b29fab
Mikelis Kreilis <mkreilis@corsair.cs.iupui.edu>
2018-11-05 16:29:10 -0500

    Added datatables links in header/footer, created listUsers.php

--- header.php
+++ header.php
@@ -16,6 +16,8 @@ require_once 'mail/mail.class.php';
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css"/>
+
 	<!-- <link rel="stylesheet" href="css/buttons.css"> -->
 
 </head>
---
+++ listUsers.php
@@ -0,0 +1,10 @@
+<?php 
+require_once 'header.php';
+?>
+
+<div class="container">
+		<h2>List Users</h2>
+	
+</div>
+
+<?php include 'footer.php'; ?>
