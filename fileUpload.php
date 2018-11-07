bfcff44b4c7ed442dae30fb935831d9c62bc6ea6
zasaeed <zasaeed@corsair.cs.iupui.edu>
2018-11-06 23:19:01 -0500

    got files to upload to folder just have to complete the database stuff

--- fileUpload.php
+++ fileUpload.php
@@ -10,25 +10,108 @@ $user = $_SESSION['user'];
 
 $userID = $user->getID();
 
-if (isset($_POST['upload'])) {
+$currentDir = getcwd();
+$uploadPictureDirectory = "/pics/";
+$uploadResumeDirectory = "/resume/";
 
-	// $updateUserInfo = $con->prepare("UPDATE user_tbl SET email = '$email', firstName = '$firstName', lastName = '$lastName', gender = '$gender', userStatus = '$userStatus' WHERE ID = '$userID'");
-	// $updateUserInfo->execute(array());
+$errors = array();
 
-	// $success = "Update Successful";
+$picExtensions = array('jpeg', 'jpg', 'png');
+$resumeExtensions = array('docx','doc','pdf','odt','txt');
 
+$picName = "";
+$picSize = "";
+$picTmpName = "";
+$picType = "";
+$picExtension = "";
+
+$msg = "";
+
+if (isset($_FILES["picture"]["name"])) {
+	if ($_FILES['picture']['size'] != 0) {
+		$picName = $_FILES['picture']['name'];
+		$picSize = $_FILES['picture']['size'];
+		$picTmpName = $_FILES['picture']['tmp_name'];
+		$picType = $_FILES['picture']['type'];
+		$picExtension = strtolower(end(explode('.',$picName)));
+
+		$uploadPath = $currentDir . $uploadPictureDirectory . basename($picName); 
+		if (isset($_POST['submit'])) {
+
+			if (! in_array($picExtension,$picExtensions)) {
+				array_push($errors,  "This picture extension is not allowed. Please upload a JPEG or PNG file");
+			}
+
+			if ($picSize > 2000000) {
+				array_push($errors, "This picture is more than 2MB. Sorry, it has to be less than or equal to 2MB");
+			}
+
+			if (empty($errors)) {
+				$didUpload = move_uploaded_file($picTmpName, $uploadPath);
+
+				if ($didUpload) {
+					$msg = $msg."The picture " . basename($picName) . " has been uploaded\n";
+				} else {
+					$msg = $msg."An error occurred with uploading the Picture. Try again or contact the admin";
+				}
+			} else {
+				foreach ($errors as $error) {
+					echo $error."\n";
+				}
+			}
+	    }
+	}
 }
+
+if (isset($_FILES["resume"]["name"])) {
+	if ($_FILES['resume']['size'] != 0) {
+		$resumeName = $_FILES['resume']['name'];
+		$resumeSize = $_FILES['resume']['size'];
+		$resumeTmpName = $_FILES['resume']['tmp_name'];
+		$resumeType = $_FILES['resume']['type'];
+		$resumeExtension = strtolower(end(explode('.',$resumeName)));
+
+		$uploadPath = $currentDir . $uploadResumeDirectory . basename($resumeName); 
+		if (isset($_POST['submit'])) {
+
+			if (! in_array($resumeExtension,$resumeExtensions)) {
+				array_push($errors,  "This resume extension is not allowed. Please upload a .docx, .doc, .pdf, .odt, or .txtfile file");
+			}
+
+			if ($picSize > 2000000) {
+				array_push($errors, "This resume is more than 2MB. Sorry, it has to be less than or equal to 2MB");
+			}
+
+			if (empty($errors)) {
+				$didUpload = move_uploaded_file($resumeTmpName, $uploadPath);
+
+				if ($didUpload) {
+					$msg = $msg."The resume " . basename($resumeName) . " has been uploaded\n";
+				} else {
+					$msg = $msg."An error occurred with uploading the Resume. Try again or contact the admin";
+				}
+			} else {
+				foreach ($errors as $error) {
+					echo $error."\n";
+				}
+			}
+	    }
+	}
+}
+
+
 ?>
 
 <section id="editUserInfo">
 	<div id="userInfoCell">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4 col-sm-8">
+				<div class="col-12">
 					<h2>Upload Files</h2>
+					<p class="lead"><?php echo $msg; ?></p>
 				</div>
 			</div>
-			<form action="fileUpload.php" method="post">
+			<form action="fileUpload.php" method="post" enctype="multipart/form-data">
 				<div class="row mt-5">
 					<div class="col-lg-6 col-sm-12">
 						<span class="help-block">
@@ -59,7 +142,7 @@ if (isset($_POST['upload'])) {
 				</div>
 				<div class="row mt-3">
 					<div class="col-sm-2 offset-sm-10">
-						<button type="submit" class="btn btn-success btn-block" name="uploadPicture">Upload <i class="fas fa-cloud-upload-alt"></i></button>
+						<button type="submit" class="btn btn-success btn-block" name="submit" value="submit">Upload <i class="fas fa-cloud-upload-alt"></i></button>
 					</div>
 				</div>
 			</form>
