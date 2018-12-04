ed446c45f5295eb6d12bf3810d33f0e4ad171867
Mikelis Kreilis <mkreilis@corsair.cs.iupui.edu>
2018-12-04 09:37:47 -0500

    added css styling

--- downloadTables.php
+++ downloadTables.php
@@ -13,43 +13,43 @@ if($_SESSION['loggedin'] == false) {
 
     <div class="row mt-4">
         <div class="col-4">
-			<h4>User Table</h4>
+			<p class="lead">User Table</p>
             <p class="lead"><a href="saveTables.php?tag=1" class="btn btn-primary btn-sm">Download User Table</a></p>
         </div>
         <div class="col-4">
-			<h4>Address Table</h4>
+			<p class="lead">Address Table</p>
             <p class="lead"><a href="saveTables.php?tag=2" class="btn btn-primary btn-sm">Download Address Table</a></p>
         </div>
         <div class="col-4">
-			<h4>Country Table</h4>
+			<p class="lead">Country Table</p>
             <p class="lead"><a href="saveTables.php?tag=3" class="btn btn-primary btn-sm">Download Country Table</a></p>
         </div>
         <div class="col-4">
-			<h4>State Table</h4>
+			<p class="lead">State Table</p>
             <p class="lead"><a href="saveTables.php?tag=4" class="btn btn-primary btn-sm">Download State Table</a></p>
         </div>
         <div class="col-4">
-			<h4>Degree Table</h4>
+			<p class="lead">Degree Table</p>
             <p class="lead"><a href="saveTables.php?tag=5" class="btn btn-primary btn-sm">Download Degree Table</a></p>
         </div>
         <div class="col-4">
-			<h4>Education Table</h4>
+			<p class="lead">Education Table</p>
             <p class="lead"><a href="saveTables.php?tag=6" class="btn btn-primary btn-sm">Download Education Table</a></p>
         </div>
         <div class="col-4">
-			<h4>Identity Table</h4>
+			<p class="lead">Identity Table</p>
             <p class="lead"><a href="saveTables.php?tag=7" class="btn btn-primary btn-sm">Download Identity Table</a></p>
         </div>
         <div class="col-4">
-			<h4>Relationship Table</h4>
+			<p class="lead">Relationship Table</p>
             <p class="lead"><a href="saveTables.php?tag=8" class="btn btn-primary btn-sm">Download Relationship Table</a></p>
         </div>
         <div class="col-4">
-			<h4>Picture Table</h4>
+			<p class="lead">Picture Table</p>
             <p class="lead"><a href="saveTables.php?tag=9" class="btn btn-primary btn-sm">Download Picture Table</a></p>
         </div>
         <div class="col-4">
-			<h4>Resume Table</h4>
+			<p class="lead">Resume Table</p>
             <p class="lead"><a href="saveTables.php?tag=10" class="btn btn-primary btn-sm">Download Resume Table</a></p>
         </div>
     </div>
